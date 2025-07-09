4f0acd2f65a983d5a1cda580546f18b01d005a4d
Bojan Djokic <bojandjokic974@gmail.com>
2025-07-09 14:40:42 +0200

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "app_service_default_hostname" {
-  value = azurerm_app_service.app_service.default_site_hostname
-}
-
-output "app_insights_instrumentation_key" {
-  value = azurerm_application_insights.app_insights.instrumentation_key
-}
